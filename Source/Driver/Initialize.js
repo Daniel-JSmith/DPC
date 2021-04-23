f79614032fc7090e708c2027cc8c9a5505539d0a
Daniel-JSmith <danieljs@uw.edu>
2021-04-23 14:09:04 -0700

    text case

--- Source/Driver/Initialize.js
+++ Source/Driver/Initialize.js
@@ -6,26 +6,26 @@ const startSceneName = 'start';
 function initialize()
 {
     MainController.scenes = {
-        'start' : new ImageScene(['0'], 'Start', 'Assets/Images/Title.png', 250),
+        'start' : new ImageScene(['0'], 'START', 'Assets/Images/Title.png', 250),
 	
-        '0' : new StandardScene(['1'], 'Answer call', '', 'Assets/Footage/0.mp4', 'Assets/Footage/0-idl.mp4'),
+        '0' : new StandardScene(['1'], 'ANSWER CALL', '', 'Assets/Footage/0.mp4', 'Assets/Footage/0-idl.mp4'),
 	
-	'1' : new StandardScene(['2A', '2B'], 'Ask how the pain hurts others', 'Ask about history of mental illness', 'Assets/Footage/1.mp4', 'Assets/Footage/1-idl.mp4'),
+	'1' : new StandardScene(['2A', '2B'], 'ASK HOW THE PAIN HURTS OTHERS', 'ASK ABOUT HISTORY OF MENTAL ILLNESS', 'Assets/Footage/1.mp4', 'Assets/Footage/1-idl.mp4'),
 	
-	'2A' : new StandardScene(['3A', '3B'], 'Ask him to explain', 'Ask him to demonstrate', 'Assets/Footage/2A.mp4', 'Assets/Footage/2A-idl.mp4'),
-	'2B' : new StandardScene(['3B', '3C'], 'Ask him to demonstrate', "Insist it's psychosomatic", 'Assets/Footage/2B.mp4', 'Assets/Footage/2B-idl.mp4'),
+	'2A' : new StandardScene(['3A', '3B'], 'ASK HIM TO EXPLAIN', 'ASK HIM TO DEMONSTRATE', 'Assets/Footage/2A.mp4', 'Assets/Footage/2A-idl.mp4'),
+	'2B' : new StandardScene(['3B', '3C'], 'ASK HIM TO DEMONSTRATE', "INSIST IT'S PSYCHOSOMATIC", 'Assets/Footage/2B.mp4', 'Assets/Footage/2B-idl.mp4'),
 	
-	'3A' : new StandardScene(['4C', '4A'], 'Try to calm him down', 'Suggest a guided meditation', 'Assets/Footage/3A.mp4', 'Assets/Footage/3A-idl.mp4'),
-	'3B' : new StandardScene(['4B-F3B', '4C-intF3C'], 'Yes', 'No', 'Assets/Footage/3B.mp4', 'Assets/Footage/3B-idl.mp4'),
-	'3C' : new StandardScene(['4B-F3C', '4D'], 'Believe him', "It's some kind of prank", 'Assets/Footage/3C.mp4', 'Assets/Footage/3C-idl.mp4'),
+	'3A' : new StandardScene(['4C', '4A'], 'TRY TO CALM HIM DOWN', 'SUGGEST A GUIDED MEDITATION', 'Assets/Footage/3A.mp4', 'Assets/Footage/3A-idl.mp4'),
+	'3B' : new StandardScene(['4B-F3B', '4C-intF3C'], 'YES', 'NO', 'Assets/Footage/3B.mp4', 'Assets/Footage/3B-idl.mp4'),
+	'3C' : new StandardScene(['4B-F3C', '4D'], 'BELIEVE HIM', "IT'S SOME KIND OF PRANK", 'Assets/Footage/3C.mp4', 'Assets/Footage/3C-idl.mp4'),
 	
-	'4A' : new StandardScene(['5A', '5B-normal'], 'He should tell people about it', 'He should keep it a secret', 'Assets/Footage/4A.mp4', 'Assets/Footage/4A-idl.mp4'),
-	'4B-F3B' : new StandardScene(['5C-F4B-F3B', '5B-F4B-F3B'], 'He should tell people about it', 'He should keep it a secret', 'Assets/Footage/4B(F3B).mp4', 'Assets/Footage/4B(F3B)-idl.mp4'),
-	'4B-F3C' : new StandardScene(['5B-normal', '5B-normal'], 'He should tell people about it', 'He should keep it a secret', 'Assets/Footage/4B(F3C).mp4', 'Assets/Footage/4B(F3C)-idl.mp4'),
+	'4A' : new StandardScene(['5A', '5B-normal'], 'HE SHOULD TELL PEOPLE ABOUT IT', 'HE SHOULD KEEP IT A SECRET', 'Assets/Footage/4A.mp4', 'Assets/Footage/4A-idl.mp4'),
+	'4B-F3B' : new StandardScene(['5C-F4B-F3B', '5B-F4B-F3B'], 'HE SHOULD TELL PEOPLE ABOUT IT', 'HE SHOULD KEEP IT A SECRET', 'Assets/Footage/4B(F3B).mp4', 'Assets/Footage/4B(F3B)-idl.mp4'),
+	'4B-F3C' : new StandardScene(['5B-normal', '5B-normal'], 'HE SHOULD TELL PEOPLE ABOUT IT', 'HE SHOULD KEEP IT A SECRET', 'Assets/Footage/4B(F3C).mp4', 'Assets/Footage/4B(F3C)-idl.mp4'),
 	'4C-intF3C' : new AutoProgressScene(['4C'], 'Assets/Footage/4C(intF3C).mp4'),
-	'4C' : new StandardScene(['5C-normal', '5B-normal'], 'He should tell people about it', 'He should keep it a secret', 'Assets/Footage/4C.mp4', 'Assets/Footage/4C-idl.mp4'),
-	'4D' : new StandardScene(['5F', '5D'], 'Call the police', 'Beg him to stop', 'Assets/Footage/4D.mp4', 'Assets/Footage/4D-idl.mp4'),
-	'4B' : new StandardScene(['5B-F4B-F3B', '5C-F4B-F3B'], 'He should tell people about it', 'He should keep it a secret', 'Assets/Footage/4B.mp4', 'Assets/Footage/4B-idl.mp4'),
+	'4C' : new StandardScene(['5C-normal', '5B-normal'], 'HE SHOULD TELL PEOPLE ABOUT IT', 'HE SHOULD KEEP IT A SECRET', 'Assets/Footage/4C.mp4', 'Assets/Footage/4C-idl.mp4'),
+	'4D' : new StandardScene(['5F', '5D'], 'CALL THE POLICE', 'BEG HIM TO STOP', 'Assets/Footage/4D.mp4', 'Assets/Footage/4D-idl.mp4'),
+	'4B' : new StandardScene(['5B-F4B-F3B', '5C-F4B-F3B'], 'HE SHOULD TELL PEOPLE ABOUT IT', 'HE SHOULD KEEP IT A SECRET', 'Assets/Footage/4B.mp4', 'Assets/Footage/4B-idl.mp4'),
 	
 	'5A' : new AutoProgressScene(['5AEnd'], 'Assets/Footage/5A.mp4'),
 	'5B-normal' : new AutoProgressScene(['5BEnd'], 'Assets/Footage/5B(normal).mp4'),
